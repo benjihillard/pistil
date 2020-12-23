c4c7e1708fee64280eafe2b87db1b24f079b9670
Benjamin Hillard <51002483+benjihillard@users.noreply.github.com>
2020-12-23 11:27:12 -0600

    port update

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const exphbs = require('express-handlebars');
 const session = require('express-session');
 // set app an listening port
 const app = express();
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 
 app.locals.copyright = false;
